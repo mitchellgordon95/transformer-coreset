b5309a9fd32155b640cc5d265f813258bc560e02
Mitchell Gordon <mitchell.gordon95@gmail.com>
2020-05-19 14:23:37 -0400

    Adding shared-qk training, switching to ducttape summaries for some tables

---
+++ plots/ted_post_finetune.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import subprocess
+import pandas as pd
+from io import StringIO
+
+fig, axes = plt.subplots()
+
+ducttape = subprocess.Popen(["ducttape", "main.tape", "-C", "main.tconf", "-p", "ted", "summary", "post_finetune"], stdout=subprocess.PIPE)
+tabular = subprocess.Popen(['tabular'], stdin=ducttape.stdout, stdout=subprocess.PIPE)
+csv = subprocess.check_output(["grep", "-o", "^[^#]*"], stdin=tabular.stdout).decode('ascii')
+
+table = pd.read_csv(StringIO(csv), sep="\s+")
+topk = table[table["PruneType"] == "topk"]
+uniform = table[table["PruneType"] == "uniform"]
+
+axes.set_xlabel("Sparsity")
+axes.set_ylabel("Dev BLEU")
+
+axes.plot(uniform["Sparsity"], uniform["post_finetune"], label="Uniform")
+axes.plot(topk["Sparsity"], topk["post_finetune"], label="Top-K")
+axes.set_title("TED Talks German-English Translation Post Prune")
+axes.legend()
+fig.savefig(f'plots_out/sparsity_vs_acc_ted_post_finetune.png')
